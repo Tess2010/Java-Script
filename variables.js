945718ef686108854d3473f71a6c9adb3561fead
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-10-15 12:00:09 -0300

    test

---
+++ variables.js
@@ -0,0 +1,117 @@
+let name = "Teresa"; //This is how we create variable use let 
+console.log("Hello World"); // This is how we print to the screen
+
+console.log("Hello " + name); // This is how we print variable to the screen
+
+name = "Elias";
+console.log("Hello " + name);
+console.log(`Hello ${name}`); //This is using a formated string using ${variable name}
+
+
+const x = 10;
+// x = 50;
+console.log(x);
+// Pushing this to git hub
+
+// JavaScrift Variables & Printing Practice
+//Question 2
+let num1 = 5;
+let num2 = 10;
+console.log(num1 + num2);
+
+//Question 3
+let age = 57;
+console.log(`I am ${age} years old.`);
+
+//Question 4
+let isStudent = "True";
+console.log(`${isStudent}`);
+
+//Question 5
+let price = 19.99;
+console.log(`${price}`);
+
+//Question 6
+let country = "Canada";
+console.log(`${country}`);
+
+//Question 7
+let isLoggedIn = "False";
+console.log(`${isLoggedIn}`);
+
+//Question 8
+let firstName = "Teresa";
+let lastName = "Kelsie";
+console.log(`${firstName} ${lastName}`);
+
+//Question 9
+let favoriteNumber = 13;
+console.log(`My favorite number is ${favoriteNumber}`);
+
+//Question 10
+let z = 10; // Used "z" as I previously set "x" as a constant 
+let y = 20;
+z = 20;
+y = 10;
+console.log(z , y);
+
+//Question 11
+let temperature = 12;
+console.log(`The temperature is ${temperature} °C`); // Use Option+Shift + 8 for degree symbol
+
+//Question 12
+let greeting = "Hello, World!";
+console.log("Hello, World!");
+
+//Question 13
+let totalAmount = 84.57;
+console.log(`Your total amount is $${totalAmount}`);
+
+//Question 14
+let speed = 65;
+console.log(`The car is moving at ${speed} km/h`);
+
+//Question 15
+let isRaining = "False";
+console.log(`Is it raining? ${isRaining}`);
+
+//Question 16
+let favoriteFood = " Steak, baked potato, veggies and ceasar salad";
+console.log(`My favorite food is ${favoriteFood}`);
+
+//Question 17
+let height = 157.48;
+console.log(`I am ${height} cm tall`);
+
+//Question 18
+let mathScore = 82;
+console.log(`My math score is ${mathScore}`);
+
+//Question 19
+let hobbies = "reading, crafting and watching K-Dramas";
+console.log(`I love ${hobbies}`);
+
+//Question 20
+let distance = 4;
+console.log(`I walked ${distance} kilometers today`);
+
+//Question 21
+let petName = "Dominoes";
+console.log(`My pet's name is ${petName}`);
+
+//Question 22
+let length = 8;
+let width = 3;
+console.log(8 * 3);
+
+//Question 23
+let isWeekend = "True";
+console.log(`Is it the weekend? ${isWeekend}`);
+
+//Question 24
+let numberOfDays = 7;
+console.log(`There are ${numberOfDays} days in a week`);
+
+//Question 25
+let score = 82;
+console.log(` Your ${score} out of 100`);
