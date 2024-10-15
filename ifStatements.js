e3592091698a4855cdf719f24ac71359cc1e81f0
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-10-15 11:52:49 -0300

    test

---
+++ ifStatements.js
@@ -0,0 +1,152 @@
+// Practicing ifStatements in JavaScript
+
+//Question 1 
+let age = 25;
+if (age >= 18) {
+    console.log("You are an adult."); 
+}
+
+//Question 2
+let temperature = 30;
+if (temperature >= 30)  {
+    console.log("It's a hot day."); //Fixed an issue
+}
+
+//Question 3
+let score = 100;
+if(score = 100) {
+    console.log("Perfect score!");
+} 
+
+//Question 4
+let israining = "yes'";
+if(isRaining = "yes") {
+    console.log("Take an umbrella."); //Added semicolon
+}
+
+//Question 5
+let speed = 65;
+if(speed > 60) {
+    console.log("You are speeding.");//Added semicolon
+}
+
+//Question 6 
+let time = 13;
+if (time >= 12) {
+    console.log("It's the afternoon");
+}
+
+//Question 7
+let isWeekend = "yes";
+if(isWeekend = "yes") {
+    console.log("It's the weekend.");
+}
+
+//Question 8
+let temp = -2;
+if(temp <= 1) {
+    console.log("It's freezing!");
+}
+
+//Question 9
+let grade = 64;
+if(grade >= 50){
+    console.log("You passed the exam.");
+}
+
+//Question 10
+let isLoggedIn = "true";
+if(isLoggedIn = "true") {
+    console.log("Welcome back!");
+}
+
+//Question 11
+let year = 2028;
+if(year % 4 === 0){
+    console.log("It's a leap year."); //This shows how to determine a leap year
+}
+
+//Question 12
+let num1 = 5;
+let num2 = 3;
+if(num1 > num2) {
+    console.log(`${num1} is greater than ${num2}.`);
+}
+
+//Question 13
+let favoriteColor = "Blue";
+if(favoriteColor = "Blue") {
+    console.log("That's a nice color.");
+}
+
+//Question 14
+let num = 5;
+if(num % 2 !== 0) {
+    console.log("The is an odd number."); //This determines if a number is odd
+}
+
+//Question 15
+let isLightOn = "false";
+if(isLightOn = "false") {
+    console.log("The light is off.");
+}
+
+//Question 16
+let hour = 7;
+if(hour >= 6 && hour < 12){
+    console.log("Good morning!");
+}
+
+//Question 17
+let hasKey = "yes";
+if(hasKey = "yes"){
+    console.log("You can enter the house.");
+}
+
+//Question 18
+let balance = -20.55;
+if(balance < 0) {
+    console.log("Your account is overdrawn.");
+}
+
+//Question 19
+let num3 = 8;
+if(num3 > 0){
+    console.log("The number is positive.");
+}
+
+//Question 20
+let month = "December";
+if(month = "December"){
+    console.log("It's the holiday season.");
+}
+
+//Question 21
+let age1 = 16;
+if(age1 < 18){
+    console.log("You are a minor.");
+}
+
+//Question 22
+let distance = 10;
+if(distance >= 5) {
+    console.log("You've walked a long way.");
+}
+
+//Question 23
+let isDoorOpen = "true"; 
+if(isDoorOpen = "true") {
+    console.log("Please close the door.");
+}
+
+//Question 24
+let favoriteAnimal = "dog";
+if(favoriteAnimal = "dog") {
+    console.log("Dogs are great pets.");
+}
+
+//Question 25
+let numberOfApples = 13;
+if(numberOfApples > 10) {
+    console.log("You have a lot of apples.");
+}
