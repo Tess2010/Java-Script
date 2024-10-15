acd1c6b5a5feabd82a24563f29c49ad9206c4172
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-10-15 13:32:14 -0300

    Pushing question 5, multiplication table of 5

--- loops.js
+++ loops.js
@@ -1,17 +1,24 @@
 // Practice questions for loops in js
 //For Loops
 
-//Question 1
+//Question 1 - Count numbers from 1-10
 for (let i = 1; i <= 10; i++) {
     console.log(i) ;
 }
 
-//Question 2
+//Question 2 - Even numbers from 2-20
 for (let num = 2; num <= 20; num += 2) {
     console.log(num);
 }
 
-// Question 3
+// Question 3 - Count down from 10-1
 for (let num1 = 10; num1 >= 1; num1 --) {
     console.log(num1);
+}
+
+//Question 4 - Multiplication table of 5
+
+for (let num2 = 1; num2 <= 10; num2 ++) {
+    console.log(`5 * ${num2} = ${5 * num2} `);
+
 }
