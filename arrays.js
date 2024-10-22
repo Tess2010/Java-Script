0b44e19a63e4a1dcc5b03b3f632f03630f33a66e
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-10-22 14:11:56 -0300

    Pushing ques 5 to github

--- arrays.js
+++ arrays.js
@@ -17,6 +17,17 @@ console.log(sum);
 
 //Question 4 - Array of 5 strings use loop, print each on a new line
 let stringArray = ["Love", "Live", "Laugh", "Peace", "Faith"]; //Declare string array
-for (let i = 0; i <= stringArray.length -1; i++) {
-    console.log(stringArray[i]);
+for (let i = 0; i <= stringArray.length -1; i++) { //Create loop
+    console.log(stringArray[i]); //Print array
 }
+//Question 5 - Array of number -Print the largest
+let array3 = [1,3,5,7,9];
+let largest = array3[0];
+// Create for loop
+for (let i = 0; i < array3.length; i++){
+    //This tells me that if the number is larger than this number it now bedomes the largest number.
+    if (array3[i] > largest) {
+        largest = array3[i];
+    }
+}    
+console.log("The largest number is " + largest);
