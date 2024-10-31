3ce5f15e4d0349048cdd0d66a6b8fe667c496fa7
Tess2010 <tlkelsie67@gmail.com>
2024-10-31 12:57:27 -0300

    Pushing questions 2 & 3 to github

--- functions.js
+++ functions.js
@@ -3,3 +3,15 @@ function greet(name){
     console.log(`Hello ${name}`);
 }
 greet("Teresa");
+
+//Question 2 - Add Function
+function add(x,y){
+    return (x+y);
+}
+console.log(add (4,6));
+
+//Question 3 - Multiply function
+function multiply(a,b){
+    return (a*b);
+}
+console.log(multiply(2,3));
