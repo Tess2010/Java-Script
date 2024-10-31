eec9c584d98a65e16be2e582cbd3c71e8129f635
Tess2010 <tlkelsie67@gmail.com>
2024-10-31 13:05:26 -0300

    Pushing question 4 to github

--- functions.js
+++ functions.js
@@ -10,8 +10,14 @@ function add(x,y){
 }
 console.log(add (4,6));
 
-//Question 3 - Multiply function
+//Question 3 - Multiply Function
 function multiply(a,b){
     return (a*b);
 }
 console.log(multiply(2,3));
+
+//Question 4 - isEven Function
+function isEven(x){
+    return (x % 2 === 0);
+}
+console.log(isEven(3))
