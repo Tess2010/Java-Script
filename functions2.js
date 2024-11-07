c9dd9c7a0f2fd6258bc228159d00f2fcd787d771
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-11-07 10:58:04 -0400

    Pushed question 2 to github

--- functions2.js
+++ functions2.js
@@ -1,2 +1,13 @@
-let array = ["orange","purple","red","black","blue"];
-    console.log(array[0]);
+//Ques. 1 -Create an array of favorite color and print the first one
+let array = ["orange","purple","red","black","blue"]; //Array of avorite colors
+    console.log(array[0]);//Print first color in array
+
+//Ques. 2 -Declare an arry of #s and print the sum
+let array2 = [1,2,3,4,6,7,8,9]; //Declare an array
+let sumOfArray = 0; //Declare sum for storage
+    for(let i = 0; i < array2.length; i++){//Create for loop to go through array
+        sumOfArray += array2[i]//Get sum
+    }
+    console.log(sumOfArray);//Print sum to console
+
+
