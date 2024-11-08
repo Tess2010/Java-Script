f906ff5f8c919afc72f0db6ae138c2fa001e029e
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-11-08 12:26:56 -0400

    Added function question 7 to github

--- functions2.js
+++ functions2.js
@@ -141,3 +141,13 @@ function countVowels(string){ //Create function
     }
     return(counter) //Return the counter
 } console.log(countVowels("Peter Piper"));
+
+//Ques. 7 - isPrime
+function isPrime(num){
+    for(let i = 2; i <= Math.sqrt(num); i++){ 
+    if (num % i === 0){ 
+        return(false)
+    }
+    } return(true)
+}
+console.log(isPrime(19));
