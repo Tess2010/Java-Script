6252b5d7948c7fc3f479882a927664777a85a4d8
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-11-12 10:27:35 -0400

    First commit of Objects Practice questions

---
+++ objects.js
@@ -0,0 +1,14 @@
+        //JavaScript Objects Practice
+//Question 1. Create an object called person with properties name, age, and city. 
+//Print each property to the console.
+        //Create object
+const person ={}; 
+        //Create Properties
+person.name = "Luna Kelsie";
+person.age = 5;
+person.city = "Halifax";
+        //Print each property to the console
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
