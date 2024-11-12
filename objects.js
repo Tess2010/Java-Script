19917c17e17e2dbdf9f31266bd81381acb1322fc
Teresa <teresa@Teresas-MBP.hitronhub.home>
2024-11-12 10:48:33 -0400

    Pushing question 2

--- objects.js
+++ objects.js
@@ -12,3 +12,10 @@ console.log(person.name);
 console.log(person.age);
 console.log(person.city);
 
+// Question 2. Create an object called car with properties brand, model, and year. 
+//Print the car's brand and model in a sentence.
+const car ={};
+car.brand = "Hyundai";
+car.model = "Palisade";
+car.year = 2025;
+console.log("My dream vehicle is a " + car.year," " + car.brand);
